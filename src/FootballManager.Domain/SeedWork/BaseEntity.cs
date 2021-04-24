c4ca89a64a7f3252351157efa70fb1540573448a
Giacomo Pellegrini <g.pellegrini@bizarretheory.com>
2021-04-24 17:53:49 +0200

    Creating the first seedwork classes for DDD approach

---
+++ src/FootballManager.Domain/SeedWork/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace FootballManager.Domain.SeedWork
+{
+
+    /// <summary>
+    /// A base object for entities. For all entities will have an integer Id
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        public virtual int Id { get; protected set; }
+
+        /// <summary>
+        /// Tells if the item is already persisted or if it is a memory-only item
+        /// </summary>
+        /// <returns>True if the object was not persisted</returns>
+        public bool IsTransient()
+        {
+            return this.Id == default;
+        }
+    }
+}
---
+++ src/FootballManager.Domain/SeedWork/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballManager.Domain.SeedWork
+{
+    /// <summary>
+    /// This class indicates an aggregate root in DDD sense, so a base object of our domain.
+    /// This is an empty interface used only to mark which items might be used as roots
+    /// </summary>
+    interface IAggregateRoot
+    {
+    }
+}
